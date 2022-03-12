0a9c9500fe05d57ae422c87ae33beada39cf3b39
Mikhail Bocharov <bocharov.mihail@gmail.com>
2022-03-12 16:04:30 +0300

    wip

---
+++ keenetic/opt/etc/ndm/ifstatechanged.d/wg-trsp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+WG_IF="nwg1"
+WG_DNS="10.224.0.1"
+
+if [ ! "$system_name" == "$WG_IF" ]; then
+   exit 0
+fi
+
+if_dnat_exists=`iptables -t nat -L -n | grep "to:${WG_DNS}:53" >/dev/null ; echo -n $?`
+
+if [ "$link" == "up" ] && [ "$connected" == "yes" ]; then
+    if [ ! "$if_dnat_exists" = 0 ]; then
+    	iptables -t nat -I PREROUTING -p udp -m udp --dport 53 -j DNAT --to-destination $WG_DNS:53 2>&1 >> /tmp/hey
+    fi
+fi
+
+if [ "$connected" == "no" ]; then
+    iptables -t nat -D PREROUTING -p udp -m udp --dport 53 -j DNAT --to-destination $WG_DNS:53 2>&1 >> /tmp/hey
+fi
+
