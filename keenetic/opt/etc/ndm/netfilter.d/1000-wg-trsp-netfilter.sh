ae608b658f1b84bed9ddf8ab83ad478c72c4cee1
Mikhail Bocharov <bocharov.mihail@gmail.com>
2023-07-20 06:23:31 +0300

    wip

--- keenetic/opt/etc/ndm/netfilter.d/1000-wg-trsp-netfilter.sh
+++ keenetic/opt/etc/ndm/netfilter.d/1000-wg-trsp-netfilter.sh
@@ -3,10 +3,9 @@
 [ "$type" == "ip6tables" ] && exit 0   # check the protocol type in backward-compatible way
 [ "$table" != "nat" ] && exit 0   # check the table name
 
-
-DNSMASQ_CONF="/opt/etc/dnsmasq.conf"
-DNSMASQ_INITD="/opt/etc/init.d/S56dnsmasq"
-DNSMASQ_DNS="127.0.0.1:65053"
+#DNSMASQ_CONF="/opt/etc/dnsmasq.conf"
+#DNSMASQ_INITD="/opt/etc/init.d/S56dnsmasq"
+#DNSMASQ_DNS="127.0.0.1:65053"
 
 WG_DNS="10.224.0.1:53"
 
@@ -18,6 +17,7 @@ iptables_check_cmd="iptables -C $iptables_cmd"
 iptables_del_cmd="iptables -D $iptables_cmd"
 
 
+
 WG_IF=`ip -o -4 addr show | egrep "10\.224\.0\.[0-9]{1,3}\/" | awk '{print $2}'`
 if [ -z "$WG_IF" ]; then
     echo "wg interface for 10.224.0.0/ not found";
@@ -26,22 +26,23 @@ fi
 
 is_interface_up=`ip a show "$WG_IF" up | grep UP &>/dev/null ; echo $?`
 
-if [ "$link" == "up" ] && [ "$connected" == "yes" ]; then
+if [ "$is_interface_up" == "0" ]; then
   if ! $iptables_check_cmd; then
+    echo "Add iptables"
     `$iptables_add_cmd`
   else
     echo "PREROUTING rule already exists"
   fi
-#  if ! grep -q "server=$WG_DNS" $DNSMASQ_CONF; then
-#      sed -ie "/^strict-order/a server=$WG_DNS" $DNSMASQ_CONF
-#      $DNSMASQ_INITD restart
-#  fi
+else
+  `$iptables_del_cmd` || true
 fi
 
 
-if [ "$is_interface_up" == "1" ]; then
-    `$iptables_del_cmd` || true
+#if [ "$is_interface_up" == "1" ]; then
+#    `$iptables_del_cmd` || true
 #    if grep -q "server=$WG_DNS" $DNSMASQ_CONF; then
 #	sed -ie "/^server=$WG_DNS/d" $DNSMASQ_CONF
+#	$DNSMASQ_INITD restart
+#    fi
 #
-fi
+#fi
